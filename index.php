0e9468bfdc1e79d549c8ad22a0958a9320ec47ef
TianyangLiu <TianyangLiu@10.211.55.5>
2016-09-09 12:38:42 -0700

    added three students

--- index.php
+++ index.php
@@ -11,7 +11,51 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "hello out there...I live";
+            // include the student class
+            include('student.php');
+            
+            $students = array();
+            
+            // add the first student
+            $first = new Student();
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('home', 'john@doe.com');
+            $first->add_email('work', 'jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
+            
+            // add the second student
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home', 'albert@braniacs.com');
+            $second->add_email('work1', 'a_einstein@bcit.com');
+            $second->add_email('work2', 'albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
+            
+            // add the third student
+            $third = new Student();
+            $third->surname = "Liu";
+            $third->first_name = "Tianyang";
+            $third->add_email('home', 'lty0711@qq.com');
+            $third->add_email('school', 'anyyhmy@gamil.com');
+            $third->add_grade(60);
+            $third->add_grade(65);
+            $third->add_grade(75);
+            $students['t789'] = $third;
+            
+            ksort($students); // sort the output
+            
+            // print every students' info
+            foreach($students as $student){
+                echo $student->toString();
+            }
         ?>
     </body>
 </html>
